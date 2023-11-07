004dab69fcd652e734144b4c5ad5526cf204db5f
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2023-11-07 14:36:25 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './Signup'
+import Login from './Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element = {<Signup />}></Route>
+        <Route path='/login' element = {<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
